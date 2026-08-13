d1b8fc36096b57b9ebb640263d53db9bde45de60
grox <is4cm0tt4@proton.me>
2026-08-13 20:33:56 -0300

    .

---
+++ main.py
@@ -0,0 +1,21 @@
+import ollama
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def chat():
+	return render_template("index.html")
+
+@app.route("/response")
+def response():
+	message = request.args.get("message")
+	response = ollama.chat(
+		model="smollm2:135m",
+		messages=[{"role": "user", "content":message}]
+	)
+	return jsonify({"response":response["message"]["content"]})
+
+
+if __name__ == "__main__":
+	app.run(debug=False)
